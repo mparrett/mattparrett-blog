6380dff51f6b176d6931f9512e5a69d94e4c28df
Matt Parrett <matt@plainvanillagames.com>
2015-04-17 20:08:47 -0700

    Update resume and main page

--- resources/templates/pages/about.md
+++ resources/templates/pages/about.md
@@ -1,55 +1,69 @@
-<h2 class="blog-post-title">Hi, I'm Matt. Nice to meet you!</h2>
+<h2 class="blog-post-title">Welcome! I'm Matt. <br/>Thanks for stopping by.</h2>
+<br />
+Ah yes, I've been told I should introduce myself. These days I'm a husband, father of two, technology enthusiast, adventurer, and lifelong learner.
 
-I'm a husband, father of two, adventurer, and lifelong learner.
+My mom was kind enough to bring me into the world in 1981 and I've been programming since I was 11. My interests are pretty eclectic and usually focused on technology but range from foreign language and culture to Brazilian Jiu Jitsu.
 
-I was born in 1981 and have been programming since I was 11. My interests are pretty eclectic, usually focused on technology but ranging from foreign language and culture to Brazilian Jiu Jitsu.
+This blog is an unfinished but constantly evolving experiment in sharing my life, interests, and insights. It runs on a toy PHP framework of mine and these pages are rendered Markdown. You'll also probably find some of my early experiments with personal development and writing. They're pretty bad and I've been meaning to take them down. Things aren't quite right yet, so please excuse the mess. 
 
-This blog is an evolving experiment in sharing my life, interests, and insights. New adventures are always in progress, so stay tuned for updates.
+So yeah, life. What's the journey been like so far?
 
-A few life highlights:
-
-- 1981: Entered the world...
-- 2006: Quit my call center job to pursue a career in programming...
-- 2008: Started training Brazilian Jiu Jitsu with Gustavo Dantas...
-- 2009: Achieved a childhood dream of programming computer games...
-- 2010: Participated in my first Monastic Challenge...
-- 2011: Married my beautiful wife Rachael...
-- 2012: Met my first son Micah...
-- 2014: Met my second son Eli...
+- 1981: Entered the world
+- 1994: Tried my hand at building a 3D engine and fighting game 
+- 1999: Experienced my first failed business attempt
+- 2001: Dropped out of college to work and experiment with business
+- 2006: Quit my customer service job to pursue a career in programming
+- 2008: Started training Brazilian Jiu Jitsu with Gustavo Dantas
+- 2009: Achieved a childhood dream of coding real computer games
+- 2010: Participated in my first Monastic Challenge
+- 2011: Married my beautiful wife Rachael
+- 2012: Became a dad. Blessed with my first son Micah
+- 2014: Met my second son Eli
+- 2015: Earned my purple belt in BJJ
 - 2015: ???
 
-**Some interests...**
+**Some of my interests:**
 
-- Software Engineering
+- Software Development
   - PHP
   - Python
   - Bash
   - ActionScript
   - Haxe
 - Systems
+  - Docker
   - Ubuntu
   - RedHat
   - MySQL
-- Language learning
-  - Spanish
-  - Japanese
-  - Chinese
-  - Icelandic
+  - Percona
 - Games
   - Video games
   - Chess, board games
   - Virtual reality, Oculus Rift
 - Martial Arts
   - Jiu Jitsu
-- Movies
+  - Mental toughness
+  - Physical fitness 
+- Language learning
+  - Japanese
+  - Spanish
+  - Icelandic
+  - Chinese
 - Books
+  - Man's Search for Meaning 
+  - The Obstacle is the Way
+- Movies
+  - Science-fiction
+- Outdoors
+  - Hiking
 - Travel
   - Philippines
   - Japan
   - Thailand
   - Iceland
 - Business
-  - Entrepreneurship
+  - Consulting
+  - Websites
   - Terra-P, Inc.
 
 Thanks for stopping by. Hope you find something interesting!
--- resources/templates/pages/resume.php
+++ resources/templates/pages/resume.php
@@ -10,7 +10,7 @@
 <h3 class="blog-post-title">Expert Linux Systems Engineer</h3>
 
 <p>
-Top performer with 10+ years industry experience. Dedicated team member, lifelong learner, technology enthusiast, open source software supporter, and creative developer.
+Top performer with broad skillset and 10+ years industry experience developing and scaling Linux systems. Dedicated team member and lifelong learner. Passionate about technology, automation, and consistently producing results beyond expectations.
 </p>
 
 <hr>
@@ -18,27 +18,24 @@ Top performer with 10+ years industry experience. Dedicated team member, lifelon
 <br />
 <div class="resume-exp-section">
     <h3>Chief Technology Officer &middot; Broken Bulb Game Studios</h3>
-    <em>Scottsdale, AZ / Telecommute &middot; Nov 2009 - Dec 2013, April 2014 - Present</em>
+    <em>Scottsdale, AZ / Telecommute    &middot;    Nov 2009 - Dec 2013, April 2014 - Present</em>
     <br /><br />
 
-    <p>
-    Made key decisions for all aspects of social game infrastructure during 10 product launches, supporting 300,000 daily players and tens of millions of accounts. Reduced expenses by $15,000/month by consolidating underutilized servers and systems. Designed and deployed an autoscaling private cloud environment and managed a hybrid Linux cluster of 30+ servers and virtual machines. Utilized HAProxy for load balancing and Nagios and Cacti for monitoring. Administered high traffic sharded Percona MySQL database installation. Used Chef, Ansible, and custom utilities for configuration management.
+	<p>
+	Responsible for social game infrastructure and code across 10+ game launches with 300,000+ daily players, tens of millions of total accounts, and many terabytes of player data.
     </p>
 
-    <p>
-    Designed and developed solutions from high volume automated e-mail systems to game engines and servers. Improved team effectiveness by creating custom admin utilities and analytics to reveal business insights. Co-authored a multi-process Python/Twisted game server supporting thousands of concurrent games per machine. Created custom bash scripts for a wide range of problems including deployment systems. Wrote jQuery and CSS for back-end admin tools. Utilized git and Mercurial for source code management.
+	<p>
+	Designed an autoscaling private cloud of dozens of VMs and bare metal servers hosted at SoftLayer. Scaled a variety of systems including MySQL, Memcached, NGiNX, Apache, and Couchbase. Built a custom sharding system for Percona MySQL. Automated infrastructure, configuration management, and deployment using Chef, Ansible, and custom utilities. Monitored production systems using Graphite, Cacti, Nagios, and Datadog. Developed PHP/MySQL admin utilities from automated high volume e-mailers to game engines. Created custom admin interfaces to reveal new insights about player purchasing patterns. Co-authored a game server (Python/Twisted) supporting thousands of concurrent games per instance.
     </p>
 </div>
 <br />
 <div class="resume-exp-section">
     <h3>DevOps Engineer / API Developer - Plain Vanilla Games</h3>
-    <em>Telecommute / Contract &middot; Dec 2013 - Mar 2014</em>
+    <em>Telecommute / Contract    &middot;    Dec 2013 - Mar 2014</em>
     <br /><br />
-    <p>
-    Developed cryptographic receipt verification for Google Play Store and contributed other minor features to the QuizUp API (Python/Flask).
-    </p>
-    <p>
-    Supported a variety of systems on Amazon Web Services, including RabbitMQ and ElasticSearch. Researched alternative deployment methods to speed up deploy cycle. Utilized git and GitHub for source code management.
+	<p>
+Developed cryptographic receipt verification for Google Play Store and contributed other features to the QuizUp API (Python/Flask). Supported RabbitMQ and ElasticSearch on Amazon Web Services. Researched alternative deployment methods to speed up deploy cycle. Collaborated remotely using Google Hangouts, Skype, HipChat, GitHub, and Trello.
     </p>
 </div>
 
@@ -46,11 +43,11 @@ Top performer with 10+ years industry experience. Dedicated team member, lifelon
 <div class="resume-exp-section">
 
     <h3>Lead Architect (LAMP) &middot; pyzam.com</h3>
-    <em>Scottsdale, AZ &middot; Sept 2006 – Nov 2009</em>
+    <em>Scottsdale, AZ    &middot;    Sept 2006 – Nov 2009</em>
     <br /><br />
 
-    <p>
-    Developed and supported custom PHP/MySQL MVC framework, enabling rapid growth to 40,000,000 monthly pageviews on modest hardware. Managed RedHat Enterprise Linux server cluster behind a Foundry ServerIron XL. Tuned hardware, software, and the kernel for maximum performance. Responded to security breaches and troubleshot network issues. Co-developed an image-sharing social network and several unique websites utilizing many third party APIs. Utilized SEO/SEM to increase traffic and acquire users.
+	<p>
+	Created a custom PHP/MySQL MVC framework, enabling rapid growth to 40,000,000 monthly pageviews on modest hardware. Administered a RedHat Enterprise Linux server cluster and Foundry ServerIron XL. Tuned hardware, software, and the kernel for maximum performance and stability. Responded to security breaches and solved networking issues. Co-developed and sold several unique websites including a viral image-sharing site utilizing many third party API integrations. Utilized SEO/SEM to increase traffic and acquire users.  
     </p>
 </div>
 
@@ -58,11 +55,11 @@ Top performer with 10+ years industry experience. Dedicated team member, lifelon
 <div class="resume-exp-section">
 
     <h3>Technical Support Facilitator / Lead Support Specialist &middot; Intuit, Inc.</h3>
-    <em>Tucson, AZ / NY / UT / Manila, Philippines &middot; Nov 2003 – Sept 2006</em>
+    <em>Tucson, AZ / NY / UT / Manila, Philippines    &middot;    Nov 2003 – Sept 2006</em>
     <br /><br />
 
-    <p>
-    Trained hundreds of technical support agents in classes of 30 for TurboTax technical support at domestic and international sites. Wrote technical training curriculum. Developed a wiki knowledge base, enabling technical support agents to share solutions. Improved HTML and CSS customer tracking forms, reducing friction in contact documentation process.
+	<p>
+	Trained hundreds of technical support agents in classes of 30 for TurboTax technical support at domestic and international sites. Wrote technical training curriculum. Developed a wiki knowledge-base, enabling technical support agents to share solutions. Improved HTML and CSS customer tracking forms, increasing compliance with contact documentation process.
     </p>
 </div>
 
